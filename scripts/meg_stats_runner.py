a52252c6f37196edbff8be44b1957aa3772efbc0
Marten van Schijndel <vanschm@ling.osu.edu>
2014-12-06 11:08:07 -0500

    meg_pilot: cleaned up/commented code to make it more useable by non-me;

--- scripts/meg_stats_runner.py
+++ scripts/meg_stats_runner.py
@@ -1,29 +1,37 @@
 # -*- coding: utf-8; python-indent: 2; -*-
-# <nbformat>3.0</nbformat>
 # This script extracts P-values and R^2 values over each frequency band and determines model fits
 
-# <markdowncell>
+# Global Vars
+# =======
+
+DEV = True #if True: analyze the dev set; if False: analyze the test set ;; DEV is defined on a sentence level using a stepsize of N ;; TEST is the complement of DEV
+devsizerecip = 3 # the reciprocal of the dev size, so devsizerecip = 3 means the dev set is 1/3 and the test set is 2/3
+
+freqsource = None # determines how the frequency bands are defined #Can be 'weiss', 'wiki', or (any other value) an interpolation of the two
+
+CLUSTER = True #if True: analyses are done over each channel; if False: channels are clustered prior to the analysis
+clustershape = (3,3) #Clustering is done based on sensor position; given (x,y), imagine a grid with x rows and y cols 
+
+SHORTTEST = True #Tests only the MEG analogues to EEG's Pz; requires CLUSTER = False
+
+LASSO = True #False #if True: use Lasso regression; if False: use ridge regression
+FINDVALS = False #If True, searches for the optimal regression alpha value; Requires LASSO = False
+regParam = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 1e+2, 2e+2, 5e+2, 1e+3, 2e+3, 5e+3] #the alpha values to try for FINDVALS
+
+RMOUTLIERS = True #Should outliers be removed? Removes outliers based on each (channel, frequency band)
+OUTDEF = 2 #outliers are defined as those stimuli with dep vars whose value is <= this many std deviations of the mean
+
+VERBOSE = False #Provide some extra output, mainly for development purposes
+FUDGE = False # this factor is used to enable and mark dubious code, which can be cleaned up later; purely for development
+
+if CLUSTER and not LASSO:
+  raise #we don't have clustering able to work with ridge regression yet.
+if LASSO and FINDVALS:
+  raise #only ridge permits exploring alpha vals
 
 # Imports
 # =======
 
-# <codecell>
-
-#disable autosave functionality;
-#This script is taxing enough, and autosaving tends to push it over the edge
-# plus, autosaving seems to zero out the file before restoring it from the backup
-# this means an autosave causing a crash will actually delete the file rather than saving it!!!
-#%autosave 0
-
-
-#basic imports
-DEV = False
-CLUSTER = False
-COMBINE_SUBJS = False
-SHORTTEST = True #for debugging purposes
-VERBOSE = False
-
-#%pylab inline
 import time
 import pickle
 import logging as L
@@ -38,6 +46,7 @@ import sklearn
 from sklearn import preprocessing
 import scipy
 import sklearn.linear_model
+from sklearn.feature_selection import chi2
 import sys
 import re
 #import statsmodels.api as sm
@@ -46,48 +55,12 @@ import statsmodels.formula.api as smf
 #pylab.rcParams['figure.figsize'] = 10,10 #change the default image size for this session
 #pylab.ion()
 
-# <codecell>
-
-#custom imports
-#%cd ../scripts
-
 # brian's prototype routines
 from protoMEEGutils import *
 import protoSpectralWinFFTMapper as specfft
 
-# <markdowncell>
-
 # Definitions
 # ===========
-
-# <codecell>
-
-# OUTLINE, 19th Nov 2014
-#
-# script for initial "signs of life" analysis of single MEG
-#
-# load in a meg file in EEGlab format
-# load in the word properties
-# choose a "languagey" channel (left-temporal, clean, with expected ERF patterns) 
-# plot some ERFs (e.g. all nouns vs all preps) as sanity check
-# do the earlier analysis of R^2 and betas due to lexical access vars, up to X words back 
-# hand over to Marten, so he can do the analysis based on syntactic embedding...
-
-
-#### SUBROUTINES ####
-
-# plot the time-scale for ERFs and other epoch figures 
-#def commonPlotProps():
-        #zeroSample = (abs(epochStart)/float(epochLength)*epochNumTimepoints)
-        #pylab.plot((0,epochNumTimepoints),(0,0),'k--')
-        #pylab.ylim((-2.5e-13,2.5e-13)) #((-5e-14,5e-14)) # better way just to get the ones it choose itself?
-        #pylab.plot((zeroSample,zeroSample),(0,0.01),'k--')
-#        pylab.xticks(numpy.linspace(0,epochNumTimepoints,7),epochStart+(numpy.linspace(0,epochNumTimepoints,7)/samplingRate))
-#        pylab.xlabel('time (s) relative to auditory onset') #+refEvent)
-#        pylab.xlim((62,313))
-#        pylab.show()
-#        pylab.axhline(0, color='k', linestyle='--')
-#        pylab.axvline(125, color='k', linestyle='--')
         
 # adjust R2 down for the artificial inflation you get by increasing the number of explanatory features
 def adjustR2(R2, numFeatures, numSamples):
@@ -101,27 +74,15 @@ def mynormalise(A):
   A[numpy.isnan(A)] = 0
   return A
 
-# <markdowncell>
-
 # Preprocessing
 # =============
-
-# <markdowncell>
-
-# Input Params
-# ----------
-
-# <codecell>
-
-#change to the data directory to load in the data
-#%cd ../MEG_data
 
 #choose a file - I found participant V to be pretty good, and 0.01 to 50Hz filter is pretty conservative #
 (megFileTag1, megFile1) = ('V_TSSS_0.01-50Hz_@125', '../MEG_data/v_hod_allRuns_tsss_audiobookPrepro_stPad1_lp50_resamp125_frac10ICAed.set')#_hp0.010000.set')
 (megFileTag2, megFile2) = ('A_TSSS_0.01-50Hz_@125', '../MEG_data/aud_hofd_a_allRuns_tsss_audiobookPrepro_stPad1_lp50_resamp125_frac10ICAed_hp0.010000.set')
 (megFileTag3, megFile3) = ('C_TSSS_0.01-50Hz_@125', '../MEG_data/aud_hofd_c_allRuns_tsss_audiobookPrepro_stPad1_lp50_resamp125_frac10ICAed_hp0.010000.set')
 
-## put your on properties in here, as a .tab file of similar format (tab delimited, and field names in a first comment line - should be easy to do in excel...)
+## put your own properties in here, as a .tab file of similar format (tab delimited, and field names in a first comment line - should be easy to do in excel...)
 #to get the V5.tab:
 #  python ../scripts/buildtab.py hod_JoeTimes_LoadsaFeaturesV3.tab hod.wsj02to21-comparativized-gcg15-1671-4sm.fullberk.parsed.gcgbadwords > hod_JoeTimes_LoadsaFeaturesV4.tab
 #  python ../scripts/addsentid.py hod_JoeTimes_LoadsaFeaturesV4.tab > hod_JoeTimes_LoadsaFeaturesV5.tab
@@ -136,27 +97,15 @@ tokenProps = scipy.genfromtxt(tokenPropsFile,
 tokenPropsPickle = tokenPropsFile+'.cpk'
 cPickle.dump(tokenProps, open(tokenPropsPickle, 'wb'))
 
-# <markdowncell>
-
-# Trial Params
-# ------------
-
-# <codecell>
-
 triggersOfInterest=['s%d' % i for i in range(1,10)]
 refEvent = 'onTime' #,'offTime']
 # guess an epoch of -0.5 to +1s should be enough #
 epochStart = -1; # stimulus ref event
 epochEnd = +2; # 
 epochLength = epochEnd-epochStart;
-baseline = False #[-1,0]
-
-# <markdowncell>
 
 # Epoch Data
 # ----------
-
-# <codecell>
 
 # Get the goods on subject 1
 (contSignalData1, metaData1, trackTrials, tokenPropsOrig, audioSignal, samplingRate, numChannels) = loadBookMEGWithAudio(megFile1, tokenPropsPickle, triggersOfInterest, epochEnd, epochStart, icaComps=False)
@@ -179,24 +128,23 @@ wordTrialsBool = numpy.array([p != '' for p in tokenProps['stanfPOS']])
 parsedTrialsBool = numpy.array([d != -1 for d in tokenProps['syndepth']])
 #print(parsedTrialsBool[:10])
 
-# <codecell>
-
 # Set up the dev and test sets
-devsizerecip = 3 # the reciprocal of the dev size, so devsizerecip = 3 means the dev set is 1/3 and the test set is 2/3
 devitems = numpy.arange(1,max(tokenProps['sentid']),devsizerecip)
+if FUDGE:
+  devitems = numpy.arange(1,max(tokenProps['sentid']),devsizerecip*2) #split dev set in half
 devTrialsBool = numpy.array([s in devitems for s in tokenProps['sentid']])
 testTrialsBool = numpy.array([s not in devitems for s in tokenProps['sentid']])
+
 
 if DEV:
   inDataset = devTrialsBool
 else:
   inDataset = testTrialsBool
-freqsource = None # determines how the frequency bands are defined #Can be 'weiss', 'wiki', or an interpolation of the two 
 
-if CLUSTER: 
-  clustershape = (3,3) #bottom 6 sectors
-  xdivs = numpy.arange(float(clustershape[0])+1)
-  ydivs = numpy.arange(float(clustershape[1])+1)
+if CLUSTER:
+  # given an (x,y) specification, define x rows and y cols for clustering
+  xdivs = numpy.arange(float(clustershape[1])+1) # columns are defined by slicing at x values
+  ydivs = numpy.arange(float(clustershape[0])+1) # rows are defined by slicing at y values
 
   #find xmax/min and ymax/min in first loop
   minx = None
@@ -216,13 +164,16 @@ if CLUSTER:
     if not maxy or thisy > maxy:
       maxy = thisy
 
-  #print minx, maxx, miny, maxy
-  #sys.stderr.write('x: '+str(xdivs)+' y: '+str(ydivs)+'\n')
+  if VERBOSE:
+    print minx, maxx, miny, maxy
+    print 'x: '+str(xdivs)+' y: '+str(ydivs)
   xdivs *= float(maxx-minx)/float(clustershape[0])
   xdivs += minx
   ydivs *= float(maxy-miny)/float(clustershape[1])
   ydivs += miny
-  #sys.stderr.write('x: '+str(xdivs)+' y: '+str(ydivs)+'\n')
+  if VERBOSE:
+    print 'x: '+str(xdivs)+' y: '+str(ydivs)
+
   #assign sensor labels to clusters in second loop
   sensorCluster = {}
   for sensorix,sensor in enumerate(metaData1.chanlocs):
@@ -258,12 +209,10 @@ clusterpower = {}
 clustersize = {}
 FIRSTCLUSTER = True
 if SHORTTEST:
-  #chanixes = range(2)+range(metaData1.chanlocs.shape[0]-2,metaData1.chanlocs.shape[0]-4,-1)
   chanixes = [i for i,c in enumerate(metaData1.chanlocs) if c.labels[:-1] == 'MEG224'] #minus 1 because the last 'channel' is MISC
 else:
   chanixes = range(metaData1.chanlocs.shape[0]-1) #minus 1 because the last 'channel' is MISC
-#print chanixes
-#raise
+
 for channelix in chanixes: 
   print 'Compiling data from channel:',channelix
   #need to reshape because severalMagChannels needs to be channel x samples, and 1-D shapes are flattened by numpy
@@ -279,35 +228,27 @@ for channelix in chanixes:
   severalMagChannels3 = contSignalData3[channelix,:].reshape((1,-1))
   (wordTrials3, epochedSignalData3, epochSliceTimepoints, wordTimesAbsolute, numTrials, epochNumTimepoints) = wordTrialEpochify(severalMagChannels3, samplingRate, tokenPropsOrig, trackTrials, refEvent, epochEnd, epochStart)
 
-# <codecell>
-
   if True:
-#  if COMBINE_SUBJS:
     epochedSignalData = ( numpy.concatenate((epochedSignalData1,epochedSignalData2,epochedSignalData3), axis=0) )
-#  else:
-    #epochedSignalDataCore = (epochedSignalData1,epochedSignalData2,epochedSignalData3)
-  #for i,epochedSignalData in enumerate(epochedSignalDataCore):
-  
-    #print tokenProps.shape,epochedSignalData.shape
-    #sys.stderr.write(str(epochedSignalData.shape)+' '+str(parsedTrialsBool.shape) +' ' + str(inDataset.shape)+'\n')#& parsedTrialsBool & inDataset]
+
     wordEpochs = epochedSignalData[wordTrialsBool & parsedTrialsBool & inDataset]
     wordFeatures = tokenProps[wordTrialsBool & parsedTrialsBool & inDataset]
+
+  # Spectrally decompose the epochs
+  #  freqres = 32 (narrow window) freqres = 0 (wide, epoch-length window) freqres=220 (medium window?)
+    (mappedTrialFeatures, spectralFrequencies) = specfft.mapFeatures(wordEpochs,samplingRate,windowShape='hann',featureType='amp',freqRes=0)
+
   # The FFT script collapses across channels
   # index0: epoch
   # index1: channels x fft_feature_types x frequencies
   # solution: reshape the output as (epochs,channels,-1)
   
-  
-  # Spectrally decompose the epochs
-    #freqres = 32 (narrow window) freqres = 0 (wide, epoch-length window) freqres=220 (medium window?)
-    (mappedTrialFeatures, spectralFrequencies) = specfft.mapFeatures(wordEpochs,samplingRate,windowShape='hann',featureType='amp',freqRes=0)
   # Reshape output to get epochs x channels x frequency
     mappedTrialFeatures = mappedTrialFeatures.reshape((wordEpochs.shape[0],wordEpochs.shape[1],-1))
   #  print 'FFT output: ', mappedTrialFeatures.shape, spectralFrequencies.shape
   #  raise
   
-  
-  #print(spectralFrequencies)
+  # define spectral frequency bands
     freqbands = {}
     if freqsource == 'weiss':
         #Weiss et al. 05
@@ -330,13 +271,10 @@ for channelix in chanixes:
         freqbands['beta1'] = numpy.nonzero( (spectralFrequencies >= 13) & (spectralFrequencies <= 18) )
         freqbands['beta2'] = numpy.nonzero( (spectralFrequencies >= 20) & (spectralFrequencies <= 28) )
         freqbands['gamma'] = numpy.nonzero( (spectralFrequencies >= 30) & (spectralFrequencies <= 34) )
-
-    # REGULARISATION VALUES TO TRY (e.g. in Ridge GCV)
-    regParam = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 1e+2, 2e+2, 5e+2, 1e+3, 2e+3, 5e+3]
   
   # SELECT AND DESCRIBE THE REGRESSORS WE'RE CHOOSING TO USE
-  # this strings should match the names of the fields in tokenProps
-  # here you should list the features you're choosing from your .tab file (just one?)#
+  # these strings should match the names of the fields in tokenProps
+  # here you should list the features you're choosing from your .tab file #
     features = [
      'logFreq_ANC',
      #'surprisal2back_COCA',
@@ -350,10 +288,10 @@ for channelix in chanixes:
 
     # SLOT REGRESSORS IN ONE BY ONE
     explanatoryFeatures = numpy.zeros((wordFeatures.shape)) # dummy
-    #explanatoryFeatures = numpy.array([])
+
     for feature in features:
       explanatoryFeatures = numpy.vstack((explanatoryFeatures, wordFeatures[feature]))
-    explanatoryFeatures = explanatoryFeatures[1:].T # strip zeros out again
+    explanatoryFeatures = explanatoryFeatures[1:].T # strip zero dummy out again
   
   # STEP THROUGH EACH TIME POINT IN THE EPOCH, RUNNING REGRESSION FOR EACH ONE
     avefitsig = {}
@@ -364,7 +302,8 @@ for channelix in chanixes:
 
     if CLUSTER:
       #accumulate the cluster data before running the analysis
-      # This is impractical because we can't hold all the data in memory at once...
+      
+#      This first commented approach is impractical because we can't hold all the data in memory at once...
 #      for freq in mappedTrialFeatures[0,0,:]:
 #        if not freqsY:
 #          freqsY = mappedTrialFeatures[:,0,freq]
@@ -389,9 +328,12 @@ for channelix in chanixes:
       if FIRSTCLUSTER:
         trainX = pd.DataFrame(data = mynormalise(explanatoryFeatures), columns = features)
         FIRSTCLUSTER = False
-      continue #don't run the analysis yet because we haven't accumulated over the whole cluster
-      
+      continue #don't run the analysis yet because we haven't accumulated over the whole cluster      
+
+    # If we've made it this far, we're not clustering...
     for band in freqbands:
+        if VERBOSE or FINDVALS: 
+          sys.stderr.write('Fitting: '+band+'\n')
         modelTrainingFit = []
         modelTestCorrelation = []
         modelParameters = []
@@ -413,31 +355,54 @@ for channelix in chanixes:
           else:
             freqsY = numpy.concatenate(freqsY,mappedTrialFeatures[:,0,freq],axis=1)
 
-        trainX['Y'] = numpy.mean(mynormalise(freqsY),axis=1)
+        if not FINDVALS:
+          trainX['Y'] = numpy.mean(mynormalise(freqsY),axis=1)
+          if RMOUTLIERS:
+            trainX = trainX[numpy.abs(trainX['Y']-trainX['Y'].mean())<=(OUTDEF*trainX['Y'].std())] #only retain non-outliers
+        else:
+          Y = numpy.mean(mynormalise(freqsY),axis=1).reshape(-1,1)
+        if VERBOSE:
+          print trainX.shape
 
-#        df = pd.DataFrame({'Y':trainY,'X':trainX})
-#        fitlm = pd.ols(y = df['Y'], x = df['X']) # intercept=True)
-
-        #lm = sm.OLS(trainY,trainX)
         myform = 'Y ~ '+'+'.join(features)
-#        print myform, freqsY.shape, trainX.shape
-        lm = smf.ols(formula=myform,data=trainX,missing='drop')
-        bandlm = lm.fit_regularized(alpha=0.0001) #does a Lasso fit with same regularizer as above
-        signif_of_fit = bandlm.pvalues #stat['p-value']
-        goodness_of_fit0 = bandlm.rsquared
-        goodness_of_fit1 = bandlm.rsquared_adj
-        #trainedLM = lm.fit(trainX,trainY)
-        #modelParameters.append(lm)
+
+        if LASSO:
+          lm = smf.ols(formula=myform,data=trainX,missing='drop')
+          bandlm = lm.fit_regularized(alpha=0.0001,L1_wt=1.0) #does a Lasso fit with regularizer parameterized by alpha
+          signif_of_fit = bandlm.pvalues #stat['p-value']
+          goodness_of_fit0 = bandlm.rsquared
+          goodness_of_fit1 = bandlm.rsquared_adj
+        else:
+          if not FINDVALS:
+            lm = smf.ols(formula=myform,data=trainX,missing='drop')
+            bandlm = lm.fit_regularized(alpha=0.005,L1_wt=0.0) #does a Ridge fit with regularizer parameterized by alpha
+            #bandlm = lm.fit_regularized(alpha=0.0001,L1_wt=0.0) #does a Ridge fit with regularizer parameterized by alpha
+            signif_of_fit = bandlm.pvalues
+            goodness_of_fit0 = bandlm.rsquared
+            goodness_of_fit1 = bandlm.rsquared_adj
+
+          else:
+            #we're just finding alpha via ridge; sklearn won't give us significance, so just fill output with None
+            lm = sklearn.linear_model.RidgeCV(fit_intercept=True, normalize=True, alphas=regParam)
+            lm.fit(trainX,Y)
+            sys.stderr.write('alpha: '+str(lm.alpha_)+'\n')
+            signif_of_fit = None
+            goodness_of_fit0 = None
+            goodness_of_fit1 = None
+          
+
+          
         #print(lm.score(trainX,trainY),trainX.shape[1], trainX.shape[0])
         #modelTrainingFit.append(adjustR2(lm.score(trainX,trainY), trainX.shape[1], trainX.shape[0]))
         #modelTrainingFit.append(lm.score(trainX,trainY)) #for a single feature, no punishment is necessary
         avefitsig[band] = signif_of_fit
         avefit[band] = (goodness_of_fit0,goodness_of_fit1)
+
+        #makes output file HUGE
         #fitlm[band] = bandlm
         #df_banddict[band] = trainX
 
 #        print bandlm.summary()
-#        print 'R2:',goodness_of_fit
 #        raise
         
         #bandmaxfits[band] = numpy.max(modelTrainingFit)
@@ -450,10 +415,6 @@ for channelix in chanixes:
     #maxfitresults[ metaData1.chanlocs[channelix].labels ] = bandmaxfits
   
   
-  #print(modelTrainingFit)
-  #print(numpy.sort(modelTrainingFit)[::-1])
-  #print 'ave fit: ', numpy.mean(modelTrainingFit)
-  #print 'max fit: ', numpy.max(modelTrainingFit)
 if CLUSTER:
   #now we've got all the clusters, so analyze them
   myform = 'Y ~ '+'+'.join(list(trainX.columns))
@@ -462,8 +423,15 @@ if CLUSTER:
     avefitsig = {}
     for band in clusterpower[clusterid]:
       trainX['Y'] = numpy.mean(mynormalise(clusterpower[clusterid][band]),axis=1)
-      lm = smf.ols(formula=myform,data=trainX,missing='drop')
-      bandlm = lm.fit_regularized(alpha=0.0001) #does a Lasso fit with same regularizer as above
+      if RMOUTLIERS:
+        trainXnoout = trainX[numpy.abs(trainX['Y']-trainX['Y'].mean())<=(OUTDEF*trainX['Y'].std())] #only retain non-outliers
+        lm = smf.ols(formula=myform,data=trainXnoout,missing='drop')
+      else:
+        lm = smf.ols(formula=myform,data=trainX,missing='drop')
+      if LASSO:
+        bandlm = lm.fit_regularized(alpha=0.005) #does a Lasso fit with regularizer parameterized by alpha
+      else:
+        bandlm = lm.fit_regularized(alpha=0.005, L1_wt=0.0) #does a Ridge fit with regularizer parameterized by alpha
       signif_of_fit = bandlm.pvalues #stat['p-value']
       goodness_of_fit0 = bandlm.rsquared
       goodness_of_fit1 = bandlm.rsquared_adj
--- scripts/query_stats.py
+++ scripts/query_stats.py
@@ -1,8 +1,10 @@
+#provides rough cli to quickly query stats output from meg_stats_runner.py
+
 import cPickle
 
-DEV = False
-CLUSTER = False
-SHORT = True
+DEV = True #output stats from dev data
+CLUSTER = True #output stats from cluster analysis
+SHORT = False #output stats from Pz-only analysis
 if CLUSTER:
   if DEV:
     resultsFile = 'signifresults.multifactor.dev.cluster.cpk'
@@ -15,26 +17,29 @@ else:
     else:
       resultsFile = 'sigresults.test.short.cpk'
   else:
-    pass
+    raise # we don't currently support quick analysis of a full sensor-based annalysis over all channels (too much data); use notebooks/Topomap_real for visualization capabilities
   
 rsq = cPickle.load(open(resultsFile))
 
-print rsq.keys()
+print rsq.keys() #print queryable properties
 
 if CLUSTER:
+  #output cluster coordinates
   for cluster in rsq['clustersize']:
     print cluster, rsq['clustersize'][cluster]
 
 for stat in ['p']:
   for band in ['alpha']:
     print 'Significance in', band
-    if True:
+    if not CLUSTER:
       for sensor in rsq[stat]: #['0113','0123','0112','0122','1542','1532','1543','1533']:
         #print 'MEG'+sensor, 'p =',rsq[stat]['MEG'+sensor][band]
-        print 'Cluster:', sensor, 'R2:', rsq['r2'][sensor][band]
+        print 'Sensor:', sensor, 'R2:', rsq['r2'][sensor][band]
         print rsq[stat][sensor][band]
     else:
-      for sensor in [(0,0),(1,0),(2,0),(0,1),(1,1),(2,1)]: #rsq[stat]: #['0113','0123','0112','0122','1542','1532','1543','1533']:
+      #mainly we care about the bottom 6 clusters in a (3,3) cluster setup; the front of the helmet has more air since subj heads rest towards the back
+      for sensor in [(0,0),(1,0),(2,0),(0,1),(1,1),(2,1)]:
+      #for sensor in [(1,0)]: check back central cluster in a (x,3) setup
         #print 'MEG'+sensor, 'p =',rsq[stat]['MEG'+sensor][band]
         print 'Cluster:', sensor, 'R2:', rsq['r2'][sensor][band]
         print rsq[stat][sensor][band]
