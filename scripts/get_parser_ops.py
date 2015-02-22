a20845aeed02eb7792fb6f8ac54f48aa3ad96ce4
Marten van Schijndel <vanschm@ling.osu.edu>
2015-02-22 11:04:26 -0500

    meg_gcgvalid: added scripts to insert parser ops in .tab;

---
+++ scripts/get_parser_ops.py
@@ -0,0 +1,41 @@
+#cat input.linetrees | python get_parser_ops.py > output
+#use a list as a stack to get highest spanning category for each word
+# input must be in linetrees format
+# output is a list of line-delimited word/category pairs
+
+import sys
+
+cats = []
+output  = []
+
+for line in sys.stdin.readlines():
+  line = line.strip()
+  if line == '()':
+    output.append('BADPARSE BADPARSE')
+  mycat = ''
+  myword = ''
+  build_cat = False
+  for cix,char in enumerate(line):
+    if char == '(':
+      build_cat = True
+      mycat = ''
+      myword = ''
+    elif char == ' ':
+      if build_cat:
+        #add cat to stack
+        build_cat = False
+        cats.append(mycat)
+      else:
+        #add word/cat pair to output
+        output.append(mycat + ' ' + myword)
+    elif char == ')':
+      #grab the most recent incomplete category as the current category
+      mycat = cats.pop()
+    else:
+      if build_cat:
+        #build up the category
+        mycat = mycat + char
+      else:
+        #build up the word
+        myword = myword + char
+sys.stdout.write('\n'.join(output)+'\n')
